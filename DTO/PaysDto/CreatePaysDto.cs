1e5f81ae4fd10f8ff0c970e7ba126bda41655cfd
SihamAlouani <sihamalouani2002@gmail.com>
2023-11-14 10:28:21 +0100

    add pays

---
+++ DTO/PaysDto/CreatePaysDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace DTO.PaysDto
+{
+    public class CreatePaysDto
+    {
+        public string? NomPays { get; set; }
+
+        public virtual ICollection<Ville> Villes { get; set; } = new List<Ville>();
+    }
+}
---
+++ MMCHackthon/Controllers/PaysController.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Models;
+using DTO.EvenementDto;
+using Infrastructure.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DTO.PaysDto;
+using Domain.Models;
+namespace MMCHackthon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaysController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public PaysController(IUnitOfWork unitOfWork)
+        { 
+            this.unitOfWork = unitOfWork;
+
+        }
+        [HttpGet]
+        public IActionResult Get() {
+
+
+            return Ok(unitOfWork.Pay.GetAll().Select(x=> new {x.IdPays,x.NomPays}).ToArray());
+        }
+
+        [HttpPost]
+        public IActionResult CreatePays([FromBody] CreatePaysDto ced)
+        {
+        
+            Pay pays = new Pay{ 
+                IdPays=Guid.NewGuid(),
+                NomPays=ced.NomPays
+                
+            };
+
+            unitOfWork.Pay.Add(pays);
+            unitOfWork.save();
+
+            return Ok(pays);
+        }
+
+    }
+}
